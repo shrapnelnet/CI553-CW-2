293fe3635dd9605f042e981365e8ce24edf12a6a
Tyler <shrapnelnet@protonmail.com>
2024-11-24 17:29:46 +0000

    modularize setup for access to apache stuffs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,22 @@
-module com.shr4pnel {
-    requires java.datatransfer;
+module CI {
+    exports com.shr4pnel.catalogue;
+    exports com.shr4pnel.clients;
+    exports com.shr4pnel.clients.backDoor;
+    exports com.shr4pnel.clients.cashier;
+    exports com.shr4pnel.clients.customer;
+    exports com.shr4pnel.clients.packing;
+    exports com.shr4pnel.db;
+    exports com.shr4pnel.middleware;
+    exports com.shr4pnel.orders;
+    exports com.shr4pnel.remote;
+
+    requires org.apache.logging.log4j.core;
+    requires java.sql;
+    requires org.apache.derby.client;
+    requires org.apache.derby.tools;
     requires java.desktop;
     requires java.rmi;
-    requires java.sql;
-    requires org.apache.logging.log4j;
-    opens com.shr4pnel.clients;
-    opens com.shr4pnel.logging;
+
+    opens com.shr4pnel.catalogue;
+    opens com.shr4pnel.db;
 }
