d33e2368f60f98621ef9350e4fc53276ce8453c5
Tyler <shrapnelnet@protonmail.com>
2024-11-25 22:04:31 +0000

    remove old react-dom-router impl

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,18 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />
-    }
-])
 
 const theme = createTheme({
     palette: {
@@ -20,13 +12,9 @@ const theme = createTheme({
     }
 })
 
-const queryClient = new QueryClient();
-
 createRoot(document.getElementById('root')).render(
     <ThemeProvider theme={theme}>
         <CssBaseline />
-        <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router} />
-        </QueryClientProvider>
+        <App />
     </ThemeProvider>
 )
