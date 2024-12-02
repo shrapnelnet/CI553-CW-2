83176da5fa2e162a901e5a39665cfeec9f9bf2c1
Tyler <shrapnelnet@protonmail.com>
2024-12-02 21:10:53 +0000

    Remove unused interface attribs

--- src/main/java/com/shr4pnel/middleware/StockReadWriter.java
+++ src/main/java/com/shr4pnel/middleware/StockReadWriter.java
@@ -10,16 +10,6 @@ import com.shr4pnel.catalogue.Product;
  */
 public interface StockReadWriter extends StockReader {
     /**
-     * Customer buys stock, stock level is thus decremented by amount bought.
-     *
-     * @param pNum Product number
-     * @param amount Quantity of product
-     * @return StockNumber, Description, Price, Quantity
-     * @throws com.shr4pnel.middleware.StockException if issue
-     */
-    boolean buyStock(String pNum, int amount) throws StockException;
-
-    /**
      * Adds stock (Restocks) to store.
      *
      * @param pNum Product number
@@ -27,12 +17,4 @@ public interface StockReadWriter extends StockReader {
      * @throws com.shr4pnel.middleware.StockException if issue
      */
     void addStock(String pNum, int amount) throws StockException;
-
-    /**
-     * Modifies Stock details for a given product number. Information modified: Description, Price
-     *
-     * @param detail Replace with this version of product
-     * @throws com.shr4pnel.middleware.StockException if issue
-     */
-    void modifyStock(Product detail) throws StockException;
 }
