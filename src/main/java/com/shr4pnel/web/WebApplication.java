53b720a4dabd8c0dccc7f0ea2b8b4605bb6da075
Tyler <shrapnelnet@protonmail.com>
2024-12-02 21:11:21 +0000

    Add API endpoints for backend and cashier client frontends

--- src/main/java/com/shr4pnel/web/WebApplication.java
+++ src/main/java/com/shr4pnel/web/WebApplication.java
@@ -1,5 +1,6 @@
 package com.shr4pnel.web;
 
+import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.shr4pnel.db.StockR;
 import com.shr4pnel.db.StockRW;
@@ -16,22 +17,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.UUID;
+
 @SpringBootApplication
 @RestController
 public class WebApplication {
     private static final Logger webApplicationLogger = LogManager.getLogger(WebApplication.class);
-    private LocalMiddleFactory middleFactory = new LocalMiddleFactory();
+    private final LocalMiddleFactory middleFactory = new LocalMiddleFactory();
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
 
-//    @GetMapping("/")
-//    public ResponseEntity<Resource> root() {
-//        Resource index = new ClassPathResource("/static/index.html");
-//        return ResponseEntity.ok()
-//                .body(index);
-//    }
+    //    @GetMapping("/")
+    //    public ResponseEntity<Resource> root() {
+    //        Resource index = new ClassPathResource("/static/index.html");
+    //        return ResponseEntity.ok()
+    //                .body(index);
+    //    }
 
     @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/api/stock")
@@ -51,25 +56,74 @@ public class WebApplication {
         }
         return new ResponseEntity<>(json, HttpStatus.OK);
     }
-    
+
     @CrossOrigin(origins = "http://localhost:5173")
-    @PostMapping("/api/buy")
+    @PostMapping("/api/staff/buy")
     public ResponseEntity<?> buyStock(HttpEntity<String> httpEntity) {
-        webApplicationLogger.trace("POST /api/buy");
+        webApplicationLogger.trace("POST /api/staff/buy");
         String json = httpEntity.getBody();
         BuyStockHelper stockToBuy = new Gson().fromJson(json, BuyStockHelper.class);
         StockRW srw;
+        if (stockToBuy.quantity > 99 || stockToBuy.quantity < 0)
+            return new ResponseEntity<>("you make me sick!", HttpStatus.FORBIDDEN);
         boolean success = false;
         try {
             srw = (StockRW) middleFactory.makeStockReadWriter();
             srw.addStock(stockToBuy.pNum, stockToBuy.quantity);
-            webApplicationLogger.debug("Ordering {} of {}", stockToBuy.pNum, stockToBuy.quantity);
+            webApplicationLogger.debug("Ordering {} of {}", stockToBuy.quantity, stockToBuy.pNum);
             success = true;
         } catch (StockException e) {
-            webApplicationLogger.error("Error occurred while attempting to instantiate Stock Read-Writer", e);
+            webApplicationLogger.error(
+                    "Error occurred while attempting to instantiate Stock Read-Writer", e);
         }
-        if (success)
-            return new ResponseEntity<>(null, HttpStatus.OK);
+        if (success) return new ResponseEntity<>(null, HttpStatus.OK);
         return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    @CrossOrigin(origins = "http://localhost:5173")
+    @PostMapping("/api/customer/buy")
+    public ResponseEntity<?> buyStockCustomer(HttpEntity<String> httpEntity) {
+        webApplicationLogger.trace("POST /api/customer/buy");
+        String json = httpEntity.getBody();
+        Gson gson = new Gson();
+
+        // https://google.github.io/gson/UserGuide.html#collections-examples
+        // what a doozy!
+        Type buyStockResponse = new TypeToken<Collection<BuyStockHelper>>() {}.getType();
+        Collection<BuyStockHelper> jsonArray = gson.fromJson(json, buyStockResponse);
+        StockRW srw;
+        UUID orderuuid = UUID.randomUUID();
+        assert jsonArray != null;
+
+        try {
+            srw = (StockRW) middleFactory.makeStockReadWriter();
+            srw.addOrder(orderuuid);
+        } catch (StockException e) {
+            webApplicationLogger.error("Failed to create entry in ordertable", e);
+        }
+
+        for (BuyStockHelper bsh : jsonArray) {
+            try {
+                srw = (StockRW) middleFactory.makeStockReadWriter();
+                srw.addBasket(orderuuid, bsh.pNum, bsh.quantity);
+            } catch (StockException e) {
+                webApplicationLogger.error(
+                        "Failed to instantiate stock read-writer while executing customer purchase.",
+                        e);
+                return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+//    @CrossOrigin(origins = "http://localhost:5173")
+//    @GetMapping("/api/staff/pack")
+//    public ResponseEntity<HttpEntity> getOrdersToPack() {
+//        try {
+//            StockR sr = (StockR) middleFactory.makeStockReader();
+//
+//        } catch (StockException e) {
+//            webApplicationLogger.error("Failed to create stockreader in packing manager", e);
+//        }
+//    }
 }
