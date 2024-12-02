c5df349b31b07448c882cef4060509257503fd88
Tyler <shrapnelnet@protonmail.com>
2024-12-02 21:09:36 +0000

    Remove old StockReader impls

--- src/main/java/com/shr4pnel/middleware/LocalMiddleFactory.java
+++ src/main/java/com/shr4pnel/middleware/LocalMiddleFactory.java
@@ -6,7 +6,7 @@ package com.shr4pnel.middleware;
 
 import com.shr4pnel.db.StockR;
 import com.shr4pnel.db.StockRW;
-import com.shr4pnel.orders.Order;
+//import com.shr4pnel.orders.Order;
 
 /**
  * Provide access to middle tier components. Now only one instance of each middle tier object is
@@ -18,7 +18,7 @@ import com.shr4pnel.orders.Order;
 public class LocalMiddleFactory implements MiddleFactory {
     private static StockR aStockR = null;
     private static StockRW aStockRW = null;
-    private static Order aOrder = null;
+//    private static Order aOrder = null;
 
     /**
      * Return an object to access the database for read only access. All users share this same
@@ -27,7 +27,7 @@ public class LocalMiddleFactory implements MiddleFactory {
     public StockReader makeStockReader() throws StockException {
         if (aStockR == null)
             aStockR = new StockR();
-        return aStockR;
+        return (StockReader) aStockR;
     }
 
     /**
@@ -36,12 +36,12 @@ public class LocalMiddleFactory implements MiddleFactory {
      */
     public StockReadWriter makeStockReadWriter() throws StockException {
         if (aStockRW == null) aStockRW = new StockRW();
-        return aStockRW;
+        return (StockReadWriter) aStockRW;
     }
 
-    /** Return an object to access the order processing system. All users share this same object. */
-    public OrderProcessing makeOrderProcessing() throws OrderException {
-        if (aOrder == null) aOrder = new Order();
-        return aOrder;
-    }
+//    /** Return an object to access the order processing system. All users share this same object. */
+//    public OrderProcessing makeOrderProcessing() throws OrderException {
+//        if (aOrder == null) aOrder = new Order();
+//        return aOrder;
+//    }
 }
