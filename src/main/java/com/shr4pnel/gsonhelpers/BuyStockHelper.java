bbdbc4431b697b0e3b361dc89fb85eeb725dd8ec
Tyler <shrapnelnet@protonmail.com>
2024-12-02 21:07:18 +0000

    Update GSON handler

--- src/main/java/com/shr4pnel/gsonhelpers/BuyStockHelper.java
+++ src/main/java/com/shr4pnel/gsonhelpers/BuyStockHelper.java
@@ -1,11 +1,11 @@
 package com.shr4pnel.gsonhelpers;
 
 public class BuyStockHelper {
-    public String pNum;
-    public int quantity;
+        public String pNum;
+        public int quantity;
 
-    public BuyStockHelper(String pNum, int quantity) {
-        this.pNum = pNum;
-        this.quantity = quantity;
-    }
+        public BuyStockHelper(String pNum, int quantity) {
+            this.pNum = pNum;
+            this.quantity = quantity;
+        }
 }
