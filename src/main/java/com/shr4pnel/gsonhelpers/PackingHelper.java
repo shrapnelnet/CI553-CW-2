0ecbd3199b213ed1395e3015836be3c755979c59
shrapnelnet <shrapnelnet@protonmail.com>
2024-12-15 14:44:27 +0000

    create packing helper for gson casts

--- src/main/java/com/shr4pnel/gsonhelpers/PackingHelper.java
+++ src/main/java/com/shr4pnel/gsonhelpers/PackingHelper.java
@@ -1,4 +1,17 @@
 package com.shr4pnel.gsonhelpers;
 
+import java.util.Date;
+
 public class PackingHelper {
+    private Date date;
+    private String pNum;
+    private int quantity;
+    private String UUID;
+
+    public PackingHelper(Date date, String pNum, int quantity, String UUID) {
+        this.date = date;
+        this.pNum = pNum;
+        this.quantity = quantity;
+        this.UUID = UUID;
+    }
 }
