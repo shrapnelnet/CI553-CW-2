8032b6a25a9c41d811be1e603f2239c8a5bc060a
shrapnelnet <shrapnelnet@protonmail.com>
2025-01-01 19:16:22 +0000

    prune module-info of exports and reflection opening

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,24 +1,11 @@
 module CI {
-    exports com.shr4pnel.clients;
-    exports com.shr4pnel.db;
-    exports com.shr4pnel.middleware;
-    exports com.shr4pnel.web;
-
-    requires org.apache.logging.log4j.core;
-    requires java.sql;
-    requires org.apache.derby.client;
-    requires org.apache.derby.tools;
-    requires java.desktop;
-    requires java.rmi;
+    requires com.google.common;
+    requires com.google.gson;
+    requires org.apache.logging.log4j;
     requires spring.boot;
     requires spring.boot.autoconfigure;
-    requires spring.web;
-    requires org.apache.commons.csv;
-    requires com.google.gson;
     requires spring.core;
-    requires com.google.common;
-
-    opens com.shr4pnel.db;
-    opens com.shr4pnel.web;
-
+    requires spring.web;
+    requires org.apache.derby.tools;
+    requires java.sql;
 }
