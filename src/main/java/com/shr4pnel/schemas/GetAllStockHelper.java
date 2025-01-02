5114e70f31f8b94c51d1c636e91a0f500812188c
shrapnelnet <shrapnelnet@protonmail.com>
2025-01-02 20:43:21 +0000

    Rename gsonhelpers subpackage -> schemas

---
+++ src/main/java/com/shr4pnel/schemas/GetAllStockHelper.java
@@ -0,0 +1,57 @@
+package com.shr4pnel.gsonhelpers;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "Get all stock serialization helper")
+public class GetAllStockHelper {
+    @Schema(name = "Item name", description = "Name of item", example = "32GB USB Drive")
+    private String name;
+
+    @Schema(name = "Item price", example = "7")
+    private long price;
+
+    @Schema(name = "Quantity in stock", example = "4")
+    private long stockLevel;
+
+    @Schema(name = "Product number", example = "0001")
+    private String pNum;
+
+    public GetAllStockHelper(String name, long price, long stockLevel, String pNum) {
+        this.name = name;
+        this.price = price;
+        this.stockLevel = stockLevel;
+        this.pNum = pNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public long getStockLevel() {
+        return stockLevel;
+    }
+
+    public void setStockLevel(long stockLevel) {
+        this.stockLevel = stockLevel;
+    }
+
+    public String getpNum() {
+        return pNum;
+    }
+
+    public void setpNum(String pNum) {
+        this.pNum = pNum;
+    }
+}
