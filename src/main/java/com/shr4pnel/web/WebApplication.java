c56e7189dcfb27233e57f04165a0781d595f5c20
shrapnelnet <shrapnelnet@protonmail.com>
2025-01-02 20:51:07 +0000

    Lint annotations

--- src/main/java/com/shr4pnel/web/WebApplication.java
+++ src/main/java/com/shr4pnel/web/WebApplication.java
@@ -4,10 +4,10 @@ import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.shr4pnel.db.StockR;
 import com.shr4pnel.db.StockRW;
-import com.shr4pnel.schemas.BuyStockHelper;
-import com.shr4pnel.schemas.GetAllStockHelper;
 import com.shr4pnel.middleware.LocalMiddleFactory;
 import com.shr4pnel.middleware.StockException;
+import com.shr4pnel.schemas.BuyStockHelper;
+import com.shr4pnel.schemas.GetAllStockHelper;
 import com.shr4pnel.schemas.PackingHelper;
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -30,21 +30,16 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.reflect.Type;
 import java.util.Collection;
 import java.util.UUID;
 
-@OpenAPIDefinition(
-    info = @Info(
-        title = "Catshop REST API",
-        version = "v0.1.0",
-        description = "Entrypoint for Spring-boot. Handles communication between the client and database, as well as hosting the frontend itself.",
-        license = @License(name = "GNU GPLv3", url = "https://www.gnu.org/licenses/gpl-3.0.en.html"),
-        contact = @Contact(name = "Tyler", email = "tyler@shrapnelnet.co.uk")
-    )
-)
+@OpenAPIDefinition(info = @Info(title = "Catshop REST API", version = "v0.1.0", description = "Entrypoint for Spring-boot. Handles communication between the client and database, as well as hosting the frontend itself.", license = @License(name = "GNU GPLv3", url = "https://www.gnu.org/licenses/gpl-3.0.en.html"), contact = @Contact(name = "Tyler", email = "tyler@shrapnelnet.co.uk")))
 @SpringBootApplication
 @RestController
 public class WebApplication {
@@ -59,34 +54,10 @@ public class WebApplication {
     @GetMapping("/")
     public ResponseEntity<Resource> root() {
         Resource index = new ClassPathResource("/index.html");
-        return ResponseEntity.ok()
-                .body(index);
+        return ResponseEntity.ok().body(index);
     }
 
-    @Operation(
-        summary = "Get stock level",
-        description = "Get the current amount of stock in the database, and send it to the client. Used to synchronize customer, cashier and backdoor clients.",
-        responses = {
-            @ApiResponse(
-                content = @Content(
-                    mediaType = "application/json",
-                    array = @ArraySchema(
-                            schema = @Schema(implementation = GetAllStockHelper.class),
-                            minItems = 0
-                    ),
-                    examples = {
-                        @ExampleObject(
-                            name = "Stock",
-                            description = "Get stock count",
-                            value = "[{\"name\":\"40 inch LED HD TV\",\"price\":269,\"stockLevel\":90,\"pNum\":\"0001\"},{\"name\":\"DAB Radio\",\"price\":29,\"stockLevel\":20,\"pNum\":\"0002\"},{\"name\":\"Toaster\",\"price\":19,\"stockLevel\":33,\"pNum\":\"0003\"},{\"name\":\"Watch\",\"price\":29,\"stockLevel\":10,\"pNum\":\"0004\"},{\"name\":\"Digital Camera\",\"price\":89,\"stockLevel\":17,\"pNum\":\"0005\"},{\"name\":\"MP3 player\",\"price\":7,\"stockLevel\":15,\"pNum\":\"0006\"},{\"name\":\"32Gb USB2 drive\",\"price\":6,\"stockLevel\":1,\"pNum\":\"0007\"}]"),
-                        }
-                    ),
-                description = "JSON object populated with product names, stock levels and product numbers digested by the client to display stock levels",
-                responseCode = "200"
-            ),
-            @ApiResponse(responseCode = "500", description = "This endpoint will return a HTTP 500 error if a connection to the database cannot be made for any reason.", content = @Content(schema = @Schema(hidden = true)))
-        }
-    )
+    @Operation(summary = "Get stock level", description = "Get the current amount of stock in the database, and send it to the client. Used to synchronize customer, cashier and backdoor clients.", responses = {@ApiResponse(content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = GetAllStockHelper.class), minItems = 0), examples = {@ExampleObject(name = "Stock", description = "Get stock count", value = "[{\"name\":\"40 inch LED HD TV\",\"price\":269,\"stockLevel\":90,\"pNum\":\"0001\"},{\"name\":\"DAB Radio\",\"price\":29,\"stockLevel\":20,\"pNum\":\"0002\"},{\"name\":\"Toaster\",\"price\":19,\"stockLevel\":33,\"pNum\":\"0003\"},{\"name\":\"Watch\",\"price\":29,\"stockLevel\":10,\"pNum\":\"0004\"},{\"name\":\"Digital Camera\",\"price\":89,\"stockLevel\":17,\"pNum\":\"0005\"},{\"name\":\"MP3 player\",\"price\":7,\"stockLevel\":15,\"pNum\":\"0006\"},{\"name\":\"32Gb USB2 drive\",\"price\":6,\"stockLevel\":1,\"pNum\":\"0007\"}]"),}), description = "JSON object populated with product names, stock levels and product numbers digested by the client to display stock levels", responseCode = "200"), @ApiResponse(responseCode = "500", description = "This endpoint will return a HTTP 500 error if a connection to the database cannot be made for any reason.", content = @Content(schema = @Schema(hidden = true)))})
     @GetMapping("/api/stock")
     public ResponseEntity<?> getStock() {
         webApplicationLogger.trace("GET /api/stock");
@@ -103,35 +74,12 @@ public class WebApplication {
             webApplicationLogger.error("Endpoint /api/stock failed to retrieve stock levels");
             return ResponseEntity.internalServerError().build();
         }
-        return ResponseEntity.ok()
-                .header("Content-Type", "application/json")
-                .body(json);
+        return ResponseEntity.ok().header("Content-Type", "application/json").body(json);
     }
 
-    @Operation(
-        summary = "Buy new stock",
-        description = "Adds specified amount of stock to the database",
-        responses = {
-            @ApiResponse(responseCode = "204", description = "Successful addition of new stock to database", content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "403", description = "Attempt to add less than zero of more than 99 items at a time", content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "500", description = "Server cannot connect to database", content = @Content(schema = @Schema(hidden = true)))
-        }
-    )
+    @Operation(summary = "Buy new stock", description = "Adds specified amount of stock to the database", responses = {@ApiResponse(responseCode = "204", description = "Successful addition of new stock to database", content = @Content(schema = @Schema(hidden = true))), @ApiResponse(responseCode = "403", description = "Attempt to add less than zero of more than 99 items at a time", content = @Content(schema = @Schema(hidden = true))), @ApiResponse(responseCode = "500", description = "Server cannot connect to database", content = @Content(schema = @Schema(hidden = true)))})
     @PostMapping("/api/staff/buy")
-    public ResponseEntity<?> buyStock(
-            @RequestBody(
-                description = "JSON object containing product number and quantity of product to buy",
-                content = @Content(
-                    examples = @ExampleObject(
-                        name = "Request body",
-                        description = "Stringified JSON body with product number and quantity wanted",
-                        value = "{ \"quantity\": \"5\", \"pNum\": \"0003\" }"
-                    ),
-                    schema = @Schema(implementation = BuyStockHelper.class)
-                )
-            )
-            HttpEntity<String> httpEntity
-    ) {
+    public ResponseEntity<?> buyStock(@RequestBody(description = "JSON object containing product number and quantity of product to buy", content = @Content(examples = @ExampleObject(name = "Request body", description = "Stringified JSON body with product number and quantity wanted", value = "{ \"quantity\": \"5\", \"pNum\": \"0003\" }"), schema = @Schema(implementation = BuyStockHelper.class))) HttpEntity<String> httpEntity) {
         webApplicationLogger.trace("POST /api/staff/buy");
         String json = httpEntity.getBody();
         BuyStockHelper stockToBuy = new Gson().fromJson(json, BuyStockHelper.class);
@@ -147,41 +95,21 @@ public class WebApplication {
         } catch (StockException e) {
             webApplicationLogger.error("Error occurred while attempting to instantiate Stock Read-Writer", e);
         }
-        if (success)
-            return ResponseEntity.noContent().build();
+        if (success) return ResponseEntity.noContent().build();
         return ResponseEntity.internalServerError().build();
     }
 
-    @Operation(
-        summary = "Buy products",
-        description = "Removes stock from the StockTable, and places it into the OrderTable and BasketTable respectively",
-        responses = {
-            @ApiResponse(responseCode = "204", description = "Empty response, signifies successful purchase on clientside", content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "500", description = "Server cannot connect to database", content = @Content(schema = @Schema(hidden = true)))
-        }
-    )
+    @Operation(summary = "Buy products", description = "Removes stock from the StockTable, and places it into the OrderTable and BasketTable respectively", responses = {@ApiResponse(responseCode = "204", description = "Empty response, signifies successful purchase on clientside", content = @Content(schema = @Schema(hidden = true))), @ApiResponse(responseCode = "500", description = "Server cannot connect to database", content = @Content(schema = @Schema(hidden = true)))})
     @PostMapping("/api/customer/buy")
-    public ResponseEntity<?> buyStockCustomer(
-            @RequestBody(
-                description = "JSON Object containing a basket, an array of objects, each of which have a product number and a quantity to purchase.",
-                content = @Content(
-                    examples = @ExampleObject(
-                            name = "Request body",
-                            description = "Stringified JSON body with product number and quantity wanted",
-                            value = "[{\"name\":\"DAB Radio\",\"quantity\":4,\"pNum\":\"0002\",\"price\":29},{\"name\":\"Digital Camera\",\"quantity\":2,\"pNum\":\"0005\",\"price\":89}]"
-                    ),
-                    array = @ArraySchema(schema = @Schema(implementation = BuyStockHelper.class))
-                )
-            )
-            HttpEntity<String> httpEntity
-    ) {
+    public ResponseEntity<?> buyStockCustomer(@RequestBody(description = "JSON Object containing a basket, an array of objects, each of which have a product number and a quantity to purchase.", content = @Content(examples = @ExampleObject(name = "Request body", description = "Stringified JSON body with product number and quantity wanted", value = "[{\"name\":\"DAB Radio\",\"quantity\":4,\"pNum\":\"0002\",\"price\":29},{\"name\":\"Digital Camera\",\"quantity\":2,\"pNum\":\"0005\",\"price\":89}]"), array = @ArraySchema(schema = @Schema(implementation = BuyStockHelper.class)))) HttpEntity<String> httpEntity) {
         webApplicationLogger.trace("POST /api/customer/buy");
         String json = httpEntity.getBody();
         Gson gson = new Gson();
 
         // https://google.github.io/gson/UserGuide.html#collections-examples
         // what a doozy!
-        Type buyStockResponse = new TypeToken<Collection<BuyStockHelper>>() {}.getType();
+        Type buyStockResponse = new TypeToken<Collection<BuyStockHelper>>() {
+        }.getType();
         Collection<BuyStockHelper> jsonArray = gson.fromJson(json, buyStockResponse);
         StockRW srw;
         UUID orderuuid = UUID.randomUUID();
@@ -197,37 +125,14 @@ public class WebApplication {
                 srw = middleFactory.makeStockReadWriter();
                 srw.addBasket(orderuuid, bsh.pNum, bsh.quantity);
             } catch (StockException e) {
-                webApplicationLogger.error(
-                        "Failed to instantiate stock read-writer while executing customer purchase.",
-                        e);
+                webApplicationLogger.error("Failed to instantiate stock read-writer while executing customer purchase.", e);
                 return ResponseEntity.internalServerError().build();
             }
         }
         return ResponseEntity.noContent().build();
     }
 
-    @Operation(
-        summary = "Get all items that are waiting to be packed",
-        description = "Fetch all items in OrderTable, then all items in BasketTable with a matching foreign key to aggregate the baskets correctly",
-        responses = {
-            @ApiResponse(
-                responseCode = "200",
-                description = "Successfully retrieved packing orders",
-                content = @Content(
-                    mediaType = "application/json",
-                    examples = @ExampleObject(
-                        name = "Response body",
-                        summary = "Array of orders that need to be packed",
-                        description = "Array children with identical orderIDs get merged in clientside javascript.",
-                        value = "[{\"date\":\"Jan 2, 2025\",\"pNum\":\"0002\",\"quantity\":4,\"UUID\":\"acc53edd-d266-463d-abf1-a032853c56b6\"},{\"date\":\"Jan 2, 2025\",\"pNum\":\"0005\",\"quantity\":2,\"UUID\":\"acc53edd-d266-463d-abf1-a032853c56b6\"}]"
-                    ),
-                    array = @ArraySchema(
-                        schema = @Schema(implementation = PackingHelper.class)
-                    )
-                )
-            )
-        }
-    )
+    @Operation(summary = "Get all items that are waiting to be packed", description = "Fetch all items in OrderTable, then all items in BasketTable with a matching foreign key to aggregate the baskets correctly", responses = {@ApiResponse(responseCode = "200", description = "Successfully retrieved packing orders", content = @Content(mediaType = "application/json", examples = @ExampleObject(name = "Response body", summary = "Array of orders that need to be packed", description = "Array children with identical orderIDs get merged in clientside javascript.", value = "[{\"date\":\"Jan 2, 2025\",\"pNum\":\"0002\",\"quantity\":4,\"UUID\":\"acc53edd-d266-463d-abf1-a032853c56b6\"},{\"date\":\"Jan 2, 2025\",\"pNum\":\"0005\",\"quantity\":2,\"UUID\":\"acc53edd-d266-463d-abf1-a032853c56b6\"}]"), array = @ArraySchema(schema = @Schema(implementation = PackingHelper.class))))})
     @GetMapping("/api/staff/pack")
     public ResponseEntity<?> getOrdersToPack() {
         String jsonResponse = null;
@@ -240,31 +145,16 @@ public class WebApplication {
         if (jsonResponse == null) {
             return ResponseEntity.internalServerError().build();
         }
-        return ResponseEntity.ok()
-                .header("Content-Type", "application/json")
-                .body(jsonResponse);
+        return ResponseEntity.ok().header("Content-Type", "application/json").body(jsonResponse);
     }
 
-    @Operation(
-        summary = "Delete packed orders",
-        description = "Delete orders that are sent to be packed in the packing frontend client.",
-        responses = {
-            @ApiResponse(
-                responseCode = "204",
-                description = "Successful deletion",
-                content = @Content(
-                    schema = @Schema(hidden = true)
-                )
-            )
-        }
-    )
+    @Operation(summary = "Delete packed orders", description = "Delete orders that are sent to be packed in the packing frontend client.", responses = {@ApiResponse(responseCode = "204", description = "Successful deletion", content = @Content(schema = @Schema(hidden = true)))})
     @DeleteMapping("/api/staff/finalizePack")
     public ResponseEntity<?> finishPacking(@Parameter(name = "orderid", description = "Order UUID", example = "b3d68113-2938-4763-bee9-c9276c5aa19d") String orderID) {
         try {
             StockRW srw = middleFactory.makeStockReadWriter();
             boolean success = srw.packOrder(orderID);
-            if (success)
-                return ResponseEntity.noContent().build();
+            if (success) return ResponseEntity.noContent().build();
             return ResponseEntity.internalServerError().build();
         } catch (StockException e) {
             webApplicationLogger.error("Failed to instantiate stock read-writer while deleting order.", e);
