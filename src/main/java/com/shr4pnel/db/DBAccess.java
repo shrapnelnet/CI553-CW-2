af2e49c748de129ad5444a4d10c85124f5274d12
shrapnelnet <shrapnelnet@protonmail.com>
2025-01-03 18:22:46 +0000

    Remove platform dependent DB calls, simplify DB creation in com.shr4pnel.clients.Setup

--- src/main/java/com/shr4pnel/db/DBAccess.java
+++ src/main/java/com/shr4pnel/db/DBAccess.java
@@ -1,24 +1,12 @@
 /**
  * Implements generic management of a database.
- *
- * @author Mike Smith University of Brighton
- * @version 2.0
+ * @author shrapnelnet
  */
 package com.shr4pnel.db;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.sql.DriverManager;
-
 /** Base class that defines the access to the database driver */
 public class DBAccess {
-    private static final Logger dbAccessLogger = LogManager.getLogger(DBAccess.class);
     private final String connURL = "jdbc:derby:derby";
-
-    public void loadDriver() throws Exception {
-        DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());
-    }
 
     public String urlOfDatabase() {
         return connURL;
--- src/main/java/com/shr4pnel/db/DBAccessFactory.java
+++ src/main/java/com/shr4pnel/db/DBAccessFactory.java
@@ -1,64 +1,38 @@
-/**
- * @author Mike Smith University of Brighton
- * @version 3.0
- */
 package com.shr4pnel.db;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-/** Manages the starting up of the database. The database may be Access, mySQL etc. */
-
-// Pattern: Abstract Factory
-//          Fix to be
-
+/**
+ * @author shrapnelnet
+ * @since v0.1.0
+ * Manages creation of databases, as well as instantiation of existing ones.
+ */
 public class DBAccessFactory {
     private static final Logger DBAccessFactoryLogger = LogManager.getLogger(DBAccessFactory.class);
-    private static final String database = "Derby";
-    private static String action = "";
-    private static String os;
-
-    public static void setAction(String name) {
-        action = name;
-    }
-
-    private static String setEnvironment() {
-        os = System.getProperties().getProperty("os.name");
-        String arch = System.getProperties().getProperty("os.arch");
-        String osVer = System.getProperties().getProperty("os.version");
-        String sysInfo = String.format("%s %s %s", os, osVer, arch);
-        DBAccessFactoryLogger.debug("System information: {}", sysInfo);
-        return os;
-    }
+    private boolean create = false;
 
     /**
-     * Return an object to implement system level access to the database.
-     *
-     * @return An object to provide system level access to the database
+     * No-arg constructor. Use to get access to an instance of Derby without creating a new database.
      */
+    public DBAccessFactory() {}
+
+    /**
+     * Access a new instance of Derby, with the option to create a new database.
+     * @param create Should a new database be created?
+     * @see com.shr4pnel.clients.Setup
+     */
+    public DBAccessFactory(boolean create) {
+        this.create = create;
+    }
+
+
     public DBAccess getNewDBAccess() {
-        setEnvironment();
-        DBAccessFactoryLogger.trace("Using {} as database type", database);
-        switch (database + action) {
-            case "Derby":
-                return new DerbyAccess(); // Derby
-
-            case "DerbyCreate":
-                return new DerbyCreateAccess(); // Derby & create database
-
-            case "mySQL":
-            case "mySQLCreate":
-                return new LinuxAccess(); // MySQL Linux
-
-            default:
-                DBAccessFactoryLogger.error("Instruction {} not implemented", database);
-                System.exit(0);
+        if (create) {
+            DBAccessFactoryLogger.trace("Creating new Apache Derby database");
+            return new DerbyCreateAccess();
         }
-        return new DBAccess(); // Unknown
+        DBAccessFactoryLogger.trace("Providing new Apache Derby instance");
+        return new DerbyAccess();
     }
 }
