1ccdbbae4fc96802fa66e2d0bdd21098ca0ed359
shrapnelnet <shrapnelnet@protonmail.com>
2025-01-06 15:48:41 +0000

    remove drivermanager import

--- src/main/java/com/shr4pnel/db/DerbyCreateAccess.java
+++ src/main/java/com/shr4pnel/db/DerbyCreateAccess.java
@@ -1,7 +1,5 @@
 package com.shr4pnel.db;
 
-import java.sql.DriverManager;
-
 class DerbyCreateAccess extends DBAccess {
     private static final String URLdb = "jdbc:derby:derby;create=true";
     public String urlOfDatabase() {
